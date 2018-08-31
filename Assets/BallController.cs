d8d30051ddfe84f24642bf9c8ae6c2553220399d
Yohei-T <yohei.tsukuda@outlook.jp>
2018-09-01 03:12:21 +0900

    PinBall 1回目のコミット
    発展課題まで達成。

---
+++ Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    ScoreCounter sc = new ScoreCounter();
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            sc.Score = 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            sc.Score = 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            sc.Score = 30;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            sc.Score = 50;
+        }
+    }
+}
---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+    private int point = 0;
+
+    private GameObject PointText;
+
+    public int Score
+    {
+        get
+        {
+            return point;
+        }
+
+        set
+        {
+            point += value;
+            this.PointText = GameObject.Find("PointText");
+            this.PointText.GetComponent<Text>().text = point.ToString();
+        }
+    }
+}
+
